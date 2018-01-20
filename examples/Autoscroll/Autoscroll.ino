01e8128e4ac64fc7ab8272108af6181091d35651
Black Brix <BlackBrix@users.noreply.github.com>
2018-01-20 16:47:21 +0100

    cosmetic changes

--- examples/Autoscroll/Autoscroll.ino
+++ examples/Autoscroll/Autoscroll.ino
@@ -43,12 +43,10 @@
  */
 
 // include the library code:
-//#include <LiquidCrystal.h>
 #include <Wire.h>
 #include <ST7032.h>
 
-// initialize the library with the numbers of the interface pins
-//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+// initialize the library
 ST7032 lcd;
 
 void setup() {
